ad6c2fec4d69d053acc4cf50df14546cd02a5043
SantiagoRS26 <santiagors2611@gmail.com>
2023-02-01 16:11:24 -0500

    install Firebase

---
+++ src/helpers/firebaseConfig.js
@@ -0,0 +1,11 @@
+export const  firebaseConfig = {
+    apiKey: "AIzaSyBOSaiFzZ6Z44HuZsszGjcz8i2rTldoG3s",
+    authDomain: "sistema-de-irrigacion.firebaseapp.com",
+    databaseURL: "https://sistema-de-irrigacion-default-rtdb.firebaseio.com",
+    projectId: "sistema-de-irrigacion",
+    storageBucket: "sistema-de-irrigacion.appspot.com",
+    messagingSenderId: "953237821200",
+    appId: "1:953237821200:web:d6a3c8e75c938b81877795",
+    measurementId: "G-NQKZD3YXGL",
+    databaseURL: "https://sistema-de-irrigacion-default-rtdb.firebaseio.com/"
+  };
