5befbd9f9d1b87a052f55886047798c3b621018d
SantiagoRS26 <santiagors2611@gmail.com>
2023-07-07 09:33:26 -0500

    add tailwindcss

--- src/helpers/firebaseConfig.js
+++ src/helpers/firebaseConfig.js
@@ -1,17 +1,20 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-const  firebaseConfig = {
-    apiKey: "AIzaSyBOSaiFzZ6Z44HuZsszGjcz8i2rTldoG3s",
-    authDomain: "sistema-de-irrigacion.firebaseapp.com",
-    databaseURL: "https://sistema-de-irrigacion-default-rtdb.firebaseio.com",
-    projectId: "sistema-de-irrigacion",
-    storageBucket: "sistema-de-irrigacion.appspot.com",
-    messagingSenderId: "953237821200",
-    appId: "1:953237821200:web:d6a3c8e75c938b81877795",
-    measurementId: "G-NQKZD3YXGL",
-    databaseURL: "https://sistema-de-irrigacion-default-rtdb.firebaseio.com/"
-  };
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDSxvgcXxrz96c0Slg_BBZeJ-hYiVeEIU8",
+  authDomain: "bdsr-c02ec.firebaseapp.com",
+  databaseURL: "https://bdsr-c02ec-default-rtdb.firebaseio.com",
+  projectId: "bdsr-c02ec",
+  storageBucket: "bdsr-c02ec.appspot.com",
+  messagingSenderId: "491365232955",
+  appId: "1:491365232955:web:80631204cf1c037cc2c71e"
+};
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export default app;
--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -5,79 +5,95 @@ import Header from '../components/Header'
 import Paragraph from '../components/Paragraph'
 import Button from '../components/Button'
 import { signOut, getAuth } from '@firebase/auth'
-import { Animated } from 'react-native'
-import { getDatabase, ref, onValue } from "firebase/database";
+import { Animated, View } from 'react-native'
+import { getDatabase, ref, onValue, set } from "firebase/database"
 import { State } from 'react-native-gesture-handler'
-
-
-
+import { Text } from 'react-native-paper'
 
 export default function Dashboard({ navigation }) {
-  const db = getDatabase();
-  const auth = getAuth();
-
-
+  const db = getDatabase()
+  const auth = getAuth()
 
   function signOutButton() {
-    auth.signOut();
+    auth.signOut()
     navigation.reset({
       index: 0,
       routes: [{ name: 'StartScreen' }],
     })
   }
-  const [humedad, setHumedad] = useState();
-  const [humedadSuelo, setHumedadSuelo] = useState();
-  const [temperatura, setTemperatura] = useState();
-  const [animation, setAnimation] = useState(new Animated.Value(0))
 
+  const [humedad, setHumedad] = useState()
+  const [humedadSuelo, setHumedadSuelo] = useState()
+  const [temperatura, setTemperatura] = useState()
+  const [animation, setAnimation] = useState(new Animated.Value(0))
+  const [bomba, setBomba] = useState()
 
   const handleAnimation = () => {
     Animated.timing(animation, {
-      toValue:1,
+      toValue: 1,
       duration: 1000
-    }).start( () => {
-      Animated.timing(animation,{
-        toValue:0,
+    }).start(() => {
+      Animated.timing(animation, {
+        toValue: 0,
         duration: 1000
       }).start()
     })
   }
 
-  const textInterpolation =  animation.interpolate({
-    inputRange: [0, 1],
-    outputRange:["#000000" , "#FF0000"]
-  })
-
-  const animatedStyle = {
-    color: textInterpolation
-  }
-
-  const user = auth.currentUser;
-  const starCountRef = ref(db, 'UsersData/' + user.uid + '/Datos_sensores');
+  const user = auth.currentUser
+  const starCountRef = ref(db, 'UsersData/' + user.uid + '/Datos_sensores')
 
   useEffect(() => {
     onValue(starCountRef, (snapshot) => {
-      handleAnimation();
-      setHumedad(snapshot.val().humedad);
-      setHumedadSuelo(snapshot.val().humedad_suelo);
-      setTemperatura(snapshot.val().temperatura);
-    });
-  }, State.name)
+      setHumedad(snapshot.val().humedad)
+      setHumedadSuelo(snapshot.val().humedad_suelo)
+      setTemperatura(snapshot.val().temperatura)
+      setBomba(snapshot.val().Estado_Bomba)
+    })
+  }, starCountRef)
 
+  const activateBomba = () => {
+    set(ref(db, 'UsersData/' + user.uid + '/Datos_sensores/Estado_Bomba'), 1)
+    setTimeout(() => {
+      set(ref(db, 'UsersData/' + user.uid + '/Datos_sensores/Estado_Bomba'), 0)
+    }, 10000)
+  }
 
   return (
-    <Background>
+    <Background className="flex gap-y-6">
       <Logo />
-      <Header>Empecemos</Header>
-        <Animated.Text style={animatedStyle.color}>Humedad: {humedad}</Animated.Text>
-        <Animated.Text>Humedad del suelo: {humedadSuelo}</Animated.Text>
-        <Animated.Text>Temperatura: {temperatura}</Animated.Text>
-        <Button
-          mode="outlined"
-          onPress={signOutButton}
-        >
-          Cerrar Sesion
-        </Button>
+      <Header className="text-xl font-black">Bienvenido Nuevamente {user.name}!</Header>
+
+      <View className="flex gap-y-4 justify-center items-center">
+        <View className="p-5 border-2 rounded-full border-blue-700">
+          <Animated.Text className="font-bold">Humedad: {humedad}</Animated.Text>
+        </View>
+        <View className="p-5 border-2 rounded-full border-blue-500">
+          <Animated.Text className="font-bold">Humedad del suelo: {humedadSuelo}</Animated.Text>
+        </View>
+        <View className="p-5 border-2 rounded-full border-red-500">
+          <Animated.Text className="font-bold">Temperatura: {temperatura}</Animated.Text>
+        </View>
+        <View className="p-5 border-2 rounded-full border-red-500">
+          <Animated.Text className="font-bold">Bomba {bomba == 1 ? 'activada' : 'desactivada'}</Animated.Text>
+        </View>
+      </View>
+      <Button
+        className={'rounded-3xl'}
+        loading={bomba === 1}
+        disabled={bomba === 1}
+        mode="outlined"
+        onPress={activateBomba}
+      >
+       {bomba && bomba === 1 ? "Bomba Activada" : "Activar Bomba"}
+      </Button>
+      <Button
+        className="rounded-3xl"
+        mode="outlined"
+        onPress={signOutButton}
+      >
+        Cerrar Sesión
+      </Button>
     </Background>
   )
 }
